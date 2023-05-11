a4299bdff440e7ad4cba039aab8140ee0657ee1e
YahyaMasri <yehiamasri83@gmail.com>
2023-05-11 11:29:21 +0300

    film model

---
+++ models/film.js
@@ -0,0 +1,28 @@
+import mongoose from "mongoose";
+import mongoosePaginate from "mongoose-paginate-v2";
+const { Schema, model } = mongoose;
+
+const FilmSchema = new Schema(
+  {
+    name: {
+      type: String,
+      require: true,
+    },
+    filmCategory: {
+      type: String,
+      require: true,
+    },
+    price: {
+      type: Number,
+      require: true,
+    },
+  },
+  {
+    timestamps: true,
+    collection: "Film",
+  }
+);
+
+const filmModel = model("Film", FilmSchema);
+
+export default filmModel;
