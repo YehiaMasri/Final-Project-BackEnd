7d6a2bd4b71464667116df85088812a44100b6ad
YahyaMasri <yehiamasri83@gmail.com>
2023-05-11 09:58:41 +0300

    order models

---
+++ models/orderModels.js
@@ -0,0 +1,62 @@
+import mongoose from "mongoose";
+import mongoosePaginate from "mongoose-paginate-v2";
+const { Schema, model } = mongoose;
+
+const orderSchema = new Schema(
+  {
+    user_id: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+    },
+    product_id: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Product",
+      },
+    ],
+    // state: {
+    // 	type: String,
+    // 	enum: [
+    // 		'created',
+    // 		'pending',
+    // 		'processing',
+    // 		'shipped',
+    // 		'delivered',
+    // 		'cancelled',
+    // 	],
+    // 	default: 'created',
+    // },
+    address: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    message: {
+      type: String,
+      trim: true,
+    },
+    // total: {
+    // 	type: Number,
+    // 	required: true,
+    // },
+    // review: {
+    // 	type: String,
+    // 	trim: true,
+    // },
+    // updated_price: {
+    // 	type: Number,
+    // },
+  },
+  {
+    timestamps: true,
+    collection: "Order",
+  }
+);
+orderSchema.plugin(mongoosePaginate);
+
+orderSchema.pre(["find", "findOne", "save", "create"], function () {
+  this.populate(["user_id", "product_id"]);
+});
+const orderModel = model("Order", orderSchema);
+orderModel.paginate().then({});
+export default orderModel;
--- models/userModels.js
+++ models/userModels.js
@@ -6,68 +6,68 @@ const { Schema, model } = mongoose;
 
 // validating the Email
 let validateEmail = function (email) {
-    let re = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
-    return re.test(email);
+  let re = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+  return re.test(email);
 };
 
 // user model
 const userSchema = new Schema(
-    {
-        username: {
-            type: String,
-            required: "username is required",
-            trim: true,
-        },
-        phoneNumber: {
-            type: String,
-            trim: true,
-            unique: true,
-            sparse: true,
-        },
-        email: {
-            type: String,
-            trim: true,
-            lowercase: true,
-            unique: true,
-            sparse: true,
-            validate: [validateEmail, "Please fill a valid email address"],
-            match: [
-                /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
-                "Please fill a valid email address",
-            ],
-        },
-        password: {
-            type: String,
-            required: "password is required",
-        },
-        image: {
-            type: String,
-            trim: true,
-        },
-        role: {
-            type: String,
-            enum: ["user", "admin", "superAdmin"],
-            default: "user",
-        },
+  {
+    username: {
+      type: String,
+      required: "username is required",
+      trim: true,
     },
-    {
-        timestamps: true,
-        collection: "User",
-    }
+    phoneNumber: {
+      type: String,
+      trim: true,
+      unique: true,
+      sparse: true,
+    },
+    email: {
+      type: String,
+      trim: true,
+      lowercase: true,
+      unique: true,
+      sparse: true,
+      validate: [validateEmail, "Please fill a valid email address"],
+      match: [
+        /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
+        "Please fill a valid email address",
+      ],
+    },
+    password: {
+      type: String,
+      required: "password is required",
+    },
+    image: {
+      type: String,
+      trim: true,
+    },
+    role: {
+      type: String,
+      enum: ["user", "admin", "superAdmin"],
+      default: "user",
+    },
+  },
+  {
+    timestamps: true,
+    collection: "User",
+  }
 );
 
 // hashing the password
 userSchema.pre("save", function (next) {
-    bcrypt
-        .genSalt(10)
-        .then((salt) => bcrypt.hash(this.password, salt))
-        .then((hashPassword) => {
-            this.password = hashPassword;
-            next();
-        })
-        .catch((err) => {
-            next(err);
-        });
+  bcrypt
+    .genSalt(10)
+    .then((salt) => bcrypt.hash(this.password, salt))
+    .then((hashPassword) => {
+      this.password = hashPassword;
+      next();
+    })
+    .catch((err) => {
+      next(err);
+    });
 });
 
 // adding the pagination plugin
